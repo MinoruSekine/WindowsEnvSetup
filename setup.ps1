09ee7c08dfc1c356ef950de6be75799e7207b9e2
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2020-07-30 21:08:32 +0900

    Resolve a warning by adding extras bucket even if already available.

--- setup.ps1
+++ setup.ps1
@@ -5,6 +5,19 @@ function ScoopInstallIfUnavailable($module_name) {
     scoop install $module_name
 }
 
+# Add "extra" bucket if not available.
+$extra_available=$FALSE
+$scoop_bucket_list_result=scoop bucket list
+foreach ($line in $scoop_bucket_list_result) {
+    if ($line -match "^extras$") {
+	$extra_available=$TRUE
+	break
+    }
+}
+if (!$extra_available) {
+    scoop bucket add extras
+}
+
 # Install each modules.
 $install_module_array = @('cppcheck', 'dark', 'emacs', 'innounp', 'mobaxterm', 'python')
 foreach ($command_to_install in $install_module_array) {
