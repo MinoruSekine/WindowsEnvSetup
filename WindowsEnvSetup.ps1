9cca23a5d3f364d05789d01518aa984a169b12a7
Minoru Sekine <Minoru.Sekine.JP@gmail.com>
2024-02-03 08:47:03 +0900

    Removed less frequently used apps. (#22)

--- WindowsEnvSetup.ps1
+++ WindowsEnvSetup.ps1
@@ -39,9 +39,7 @@ $install_module_array = @('cppcheck',
 			  'openjdk',
 			  'plantuml',
 			  'python',
-			  'sshfs-np',
-			  'sysinternals',
-			  'windirstat')
+			  'sysinternals')
 foreach ($command_to_install in $install_module_array) {
     ScoopInstallIfUnavailable $command_to_install
 }
